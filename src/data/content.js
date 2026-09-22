cdc6120d102ec7af4a98a40a63ad8eab07c6b54b
toduco5 <tminhduc1304@gmail.com>
2026-09-22 14:52:34 +0700

    r2e

--- src/data/content.js
+++ src/data/content.js
@@ -195,7 +195,7 @@ export const testimonials = [
     name: "Anh Tô Đức",
     role: "CEO TD-group",
     text: "Không gian làm việc họp hành thiết kế KIẾN TRÚC đã nâng cao năng suất và tinh thần đội ngũ. Một quyết định đầu tư thông minh.",
-    image: "https://i.pinimg.com/736x/7c/88/ca/7c88ca23f7cc70a156ec62a12391de19.jpg",
+    image: "/public/image/anh1.jpg",
   },
   {
     name: "Anh Oner",
